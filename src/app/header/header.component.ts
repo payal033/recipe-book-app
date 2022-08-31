3d6e339afaa7d8e6e2f72e8fb1abee64f2b189e7
Payal <payalkarwa99@gmail.com>
2022-08-31 23:24:56 +0530

    setting up routes and adding navigation to the app

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,20 +1,12 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  constructor() {}
 
-  @Output() linkSelected = new EventEmitter<String>();
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-  onSelectLink(linkname: String) {
-      this.linkSelected.emit(linkname);
-  }
-
+  ngOnInit(): void {}
 }
