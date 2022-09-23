722569ee34997196e32744dcf51afe90f399ac66
Payal <payalkarwa99@gmail.com>
2022-09-23 23:04:56 +0530

    store recipes in database

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,7 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { RecipeService } from '../recipes/recipe.service';
 
 @Injectable()
 export class DataStorageService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    // .put - overwrites existing data in database - depends on API
+    // subscribing here because header might not need this data
+    this.http
+      .put(
+        'https://recipeapp-59196-default-rtdb.firebaseio.com/recipes.json',
+        recipes
+      )
+      .subscribe((response) => console.log(response));
+  }
 }
