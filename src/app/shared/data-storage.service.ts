5d532b4628971193f0b1efeafb769a3bb5d9f638
Payal <payalkarwa99@gmail.com>
2022-09-23 23:28:30 +0530

    resolving data before loading

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from './recipe.model';
 
@@ -20,8 +21,20 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    this.http.get<Recipe[]>(this.RECIPE_URL).subscribe((recipes) => {
-      this.recipeService.setRecipes(recipes);
-    });
+    this.http
+      .get<Recipe[]>(this.RECIPE_URL)
+      .pipe(
+        map((recipes) => {
+          return recipes.map((recipe) => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : [],
+            };
+          });
+        })
+      )
+      .subscribe((recipes) => {
+        this.recipeService.setRecipes(recipes);
+      });
   }
 }
