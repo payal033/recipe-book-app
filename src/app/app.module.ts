087ca23fe4c6556a091c5ddf589fb89e5cecb15f
Payal <payalkarwa99@gmail.com>
2022-09-30 23:47:52 +0530

    auth feature module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AuthComponent } from './auth/auth.component';
+import { HttpClientModule } from '@angular/common/http';
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, AuthComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
+    AuthModule, // auth feature module
     RecipesModule, // recipe feature module
     ShoppingListModule, // shopping feature module
     SharedModule, // some shared modules, components and pipes
---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { AuthComponent } from './auth.component';
+import { SharedModule } from '../shared/shared.module';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [AuthComponent],
+  imports: [
+    SharedModule,
+    RouterModule.forChild([{ path: 'auth', component: AuthComponent }]),
+  ],
+})
+export class AuthModule {}
